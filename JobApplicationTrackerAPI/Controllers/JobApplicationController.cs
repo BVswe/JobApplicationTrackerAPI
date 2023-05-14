e227baad5f91681be103fe36f3b1687a305d39af
BVswe <brianvuongswe@gmail.com>
2023-05-13 18:48:26 -0700

    Added Get, Post, Put, Delete methods to JobApplicationController
    Added connection string to appsettings.json
    
    Updated JobApplications to have IDs

---
+++ JobApplicationTrackerAPI/Controllers/JobApplicationController.cs
@@ -0,0 +1,73 @@
+﻿using Dapper;
+using JobApplicationTrackerAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace JobApplicationTrackerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobApplicationController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public JobApplicationController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<JobApplication>>> GetAllJobApplications()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<JobApplication> applications = await SelectAllJobApplcations(connection);
+            return Ok(applications);
+        }
+
+        [HttpGet("{companyName}")]
+        public async Task<ActionResult<List<JobApplication>>> GetCompanyJobApplications(string companyName)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<JobApplication> applications = await connection.QueryAsync<JobApplication>("SELECT * FROM JobApplications WHERE Company=@cName",
+                new {cName=companyName});
+            return Ok(applications);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<JobApplication>> CreateJobApplication(JobApplication app)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+            await connection.ExecuteAsync(
+                "INSERT INTO JobApplications(Company, Position, ApplicationStatus) VALUES (@Company, @Position, @ApplicationStatus)",
+                app);
+            return Ok(await SelectAllJobApplcations(connection));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<JobApplication>>> UpdateJobApplication(JobApplication app)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync(
+                "UPDATE JobApplications SET Company=@Company, Position=@Position, ApplicationStatus=@ApplicationStatus WHERE ID=@Id",
+                app);
+            return Ok(await SelectAllJobApplcations(connection));
+        }
+
+        [HttpDelete("{applicationId}")]
+        public async Task<ActionResult<List<JobApplication>>> DeleteJobApplication(int applicationId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync(
+                "DELETE FROM JobApplications WHERE ID=@Id",
+                new {Id = applicationId});
+            return Ok(await SelectAllJobApplcations(connection));
+        }
+
+        private static async Task<IEnumerable<JobApplication>> SelectAllJobApplcations(SqlConnection connection)
+        {
+            return await connection.QueryAsync<JobApplication>("SELECT * FROM JobApplications");
+        }
+    }
+}
--- JobApplicationTrackerAPI/Models/JobApplication.cs
+++ JobApplicationTrackerAPI/Models/JobApplication.cs
@@ -2,6 +2,7 @@
 {
     public class JobApplication
     {
+        public int Id { get; set; }
         public string Company { get; set; } = string.Empty;
         public string Position { get; set; } = string.Empty;
         public string ApplicationStatus { get; set; } = string.Empty;
