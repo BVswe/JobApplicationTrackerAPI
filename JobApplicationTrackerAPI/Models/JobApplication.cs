1f31b37318244f856a17f38db33059002d3578bc
BVswe <brianvuongswe@gmail.com>
2023-05-13 18:05:18 -0700

    Added Relevant packages Created model for JobApplication

---
+++ JobApplicationTrackerAPI/Models/JobApplication.cs
@@ -0,0 +1,9 @@
+﻿namespace JobApplicationTrackerAPI.Models
+{
+    public class JobApplication
+    {
+        public string Company { get; set; } = string.Empty;
+        public string Position { get; set; } = string.Empty;
+        public string ApplicationStatus { get; set; } = string.Empty;
+    }
+}
